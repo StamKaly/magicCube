42d7c77e3c3e62b70fe1914cab9c7e31040077ed
Stam Kaly <stambugatti@gmail.com>
2016-10-01 19:00:24 +0300

    Fixed name

--- MagicSquare.py
+++ MagicSquare.py
@@ -1,39 +1,39 @@
 __author__ = 'Stam Kaly'
 __date__ = '30-9-2016'
 
-class MagicCube:
+class MagicSquare:
     def __init__(self):
-        self.magicCube = None
+        self.magicSquare = None
 
-    def generateMagicCube(self, rowsNumber):
+    def generateMagicSquare(self, rowsNumber):
         try:
             numberInt = int(rowsNumber)
         except ValueError:
             print("You can generate a Magic Cube only with a NUMBER of rows!")
-            self.magicCube = None
+            self.magicSquare = None
             return None
         unacceptableNumbers = ['0', '2', '4', '6', '8']
         if numberInt == 1:
-            self.magicCube = 1
+            self.magicSquare = 1
             return 1
         elif numberInt <= 0:
             print("You can't generate any cubes with 0 or less number of rows!")
         for number in unacceptableNumbers:
             if str(rowsNumber).endswith(number):
                 print("Sorry you can generate a Magic Cube only with uneven number of rows!")
-                self.magicCube = None
+                self.magicSquare = None
                 return None
         self.rowsNumber = rowsNumber
 
 
         self.finalNumber = numberInt * numberInt
-        self.magicCube = []
+        self.magicSquare = []
         for _ in range(numberInt):
             row = []
             for _ in range(numberInt):
                 row.append(0)
-            self.magicCube.append(row)
-        self.magicCube[0][int(numberInt/2+0.5) - 1] = 1
+            self.magicSquare.append(row)
+        self.magicSquare[0][int(numberInt/2+0.5) - 1] = 1
         currentNumber = 2
         currentRow = 0
         currentColumn = int(numberInt/2+0.5) - 1
@@ -46,8 +46,8 @@ class MagicCube:
                 workingRow = numberInt - 1
             if workingColumn > numberInt - 1:
                 workingColumn = 0
-            if self.magicCube[workingRow][workingColumn] == 0:
-                self.magicCube[workingRow][workingColumn] = currentNumber
+            if self.magicSquare[workingRow][workingColumn] == 0:
+                self.magicSquare[workingRow][workingColumn] = currentNumber
                 currentRow = workingRow
                 currentColumn = workingColumn
             else:
@@ -55,20 +55,20 @@ class MagicCube:
                     currentRow = 0
                 else:
                     currentRow += 1
-                self.magicCube[currentRow][currentColumn] = currentNumber
+                self.magicSquare[currentRow][currentColumn] = currentNumber
             currentNumber += 1
-        return self.magicCube
+        return self.magicSquare
 
 
-    def printMagicCube(self):
-        if self.magicCube == None:
+    def printMagicSquare(self):
+        if self.magicSquare == None:
             return
-        elif self.magicCube == 1:
+        elif self.magicSquare == 1:
             print("|\u0305\u03321\u0305\u0332|\u0305\u0332")
         else:
-            for magicRow in self.magicCube:
+            for magicRow in self.magicSquare:
                 row = ""
-                if self.magicCube.index(magicRow) == self.rowsNumber - 1:
+                if self.magicSquare.index(magicRow) == self.rowsNumber - 1:
                     extraCharacter = "\u0332"
                 else:
                     extraCharacter = ""
@@ -84,14 +84,14 @@ class MagicCube:
                         for number in str(magicNumber):
                             row += "{}\u0305{}".format(number, extraCharacter)
                 print("{}|\u0305{}".format(row, extraCharacter))
-            self.magicCube = None
+            self.magicSquare = None
 
 
-    def generateAndPrintMagicCube(self, rowsNumber):
-        self.generateMagicCube(rowsNumber)
-        self.printMagicCube()
+    def generateAndPrintMagicSquare(self, rowsNumber):
+        self.generateMagicSquare(rowsNumber)
+        self.printMagicSquare()
 
 
 
 if __name__ == '__main__':
-    MagicCube().generateAndPrintMagicCube(13)
+    MagicSquare().generateAndPrintMagicSquare(13)
