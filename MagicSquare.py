cf61c391a1c88ac722be2b319a069ff92e75e414
StamKaly <stambugatti@gmail.com>
2016-10-09 08:55:06 +0000

    Fully functional using both versions

--- MagicSquare.py
+++ MagicSquare.py
@@ -2,8 +2,13 @@ class MagicSquare:
     def __init__(self):
         self.magicSquare = None
 
-    def mod(self, number):
-        return number % self.numberInt
+    def makeSum(self):
+        _sum_ = 0
+        for _, row in zip(range(1), self.magicSquare):
+            for number in row:
+                _sum_ += number
+            self.sum = _sum_
+        
 
     def generateMagicSquare(self, rowsNumber):
         try:
@@ -20,12 +25,50 @@ class MagicSquare:
             print("You can't generate any squares with 0 or less number of rows!")
             self.magicSquare = None
             return None
+        unacceptableNumbers = ['0', '2', '4', '6', '8']
         self.rowsNumber = numberInt
         self.finalNumber = numberInt*numberInt
+        for number in unacceptableNumbers:
+            if str(rowsNumber).endswith(number):
+                # (c) Chris Gadzinski ;)
+                self.magicSquare = [[((2 * j + 1 + i) % numberInt) + ((6 + i + j) % numberInt) * numberInt + 1 for i in range(numberInt)] for j in range(numberInt)]
+                self.makeSum()
+                return self.magicSquare
 
-        # (c) Chris Gadzinski ;)
-        self.magicSquare = [[self.mod(2 * j + i) + self.mod(j + i) * numberInt
-            for i in range(numberInt)] for j in range(numberInt)]
+
+
+        self.magicSquare = []
+        for _ in range(numberInt):
+            row = []
+            for _ in range(numberInt):
+                row.append(0)
+            self.magicSquare.append(row)
+        self.magicSquare[0][int(numberInt/2-0.5)] = 1
+        currentNumber = 2
+        currentRow = 0
+        currentColumn = int(numberInt/2-0.5)
+        for _ in range(self.finalNumber - 1):
+            workingRow = currentRow
+            workingColumn = currentColumn
+            workingRow -= 1
+            workingColumn += 1
+            if workingRow < 0:
+                workingRow = numberInt - 1
+            if workingColumn > numberInt - 1:
+                workingColumn = 0
+            if self.magicSquare[workingRow][workingColumn] == 0:
+                self.magicSquare[workingRow][workingColumn] = currentNumber
+                currentRow = workingRow
+                currentColumn = workingColumn
+            else:
+                if currentRow + 1 > numberInt - 1:
+                    currentRow = 0
+                else:
+                    currentRow += 1
+                self.magicSquare[currentRow][currentColumn] = currentNumber
+            currentNumber += 1
+        self.makeSum()
+
         return self.magicSquare
 
     def printMagicSquare(self):
@@ -33,6 +76,7 @@ class MagicSquare:
             return
         elif self.magicSquare == 1:
             print("|\u0305\u03321\u0305\u0332|\u0305\u0332")
+            print("\n\nMagic Number: 1")
         else:
             for magicRow in self.magicSquare:
                 row = ""
@@ -53,7 +97,7 @@ class MagicSquare:
                             row += "{}\u0305{}".format(number, extraCharacter)
                 print("{}|\u0305{}".format(row, extraCharacter))
             self.magicSquare = None
-
+            print("\n\nMagic Number: {}".format(self.sum))
 
     def generateAndPrintMagicSquare(self, rowsNumber):
         self.generateMagicSquare(rowsNumber)
@@ -62,4 +106,4 @@ class MagicSquare:
 
 
 if __name__ == '__main__':
-    MagicSquare().generateAndPrintMagicSquare(2)
+    MagicSquare().generateAndPrintMagicSquare(9)
